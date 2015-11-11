a46bce9e5ec9fb83e511564ad93e11c37d440f32
xifei.wu <xifei.wu@acadine.com>
2015-11-11 18:52:07 +0800

    add file html_imports.js for LazyLoader

--- Gaia/LazyLoader/lazy_loader/lazy_loader.js
+++ Gaia/LazyLoader/lazy_loader/lazy_loader.js
@@ -44,7 +44,7 @@ var LazyLoader = (function() {
 
       // The next few lines are for loading html imports in DEBUG mode
       if (domNode.getAttribute('is')) {
-        this.load(['/shared/html_imports/html_imports.js'], function() {
+        this.load(['html_imports.js'], function() {
           HtmlImports.populate(callback);
         }.bind(this));
         return;
