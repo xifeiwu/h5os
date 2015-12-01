e1a84a7335886956d9305a384dc65f99bfcc47c4
xifei.wu <xifei.wu@acadine.com>
2015-12-01 11:16:18 +0800

    add database related code.

--- Calendar/code_log.js
+++ Calendar/code_log.js
@@ -93,5 +93,87 @@ fields: {
   }]
 }
 
+const DB_NAME = 'b2g-calendar';
+const DB_VERSION = 15;
+const STORE = Object.freeze({
+  events: 'events',
+  accounts: 'accounts',
+  calendars: 'calendars',
+  busytimes: 'busytimes',
+  settings: 'settings',
+  alarms: 'alarms',
+  icalComponents: 'icalComponents'
+});
 
-git diff event-dev local-CORE-2178 ./js/views/modify_event.js
+var connection;
+function getConnection() {
+  var req = indexedDB.open(DB_NAME, DB_VERSION);
+  req.onsuccess = function(evt) {
+    console.log('request success.');
+    connection = this.result;
+  }
+  req.onerror = function (evt) {
+    console.error("request error:", JSON.stringify(evt));
+  };
+}
+
+function showDataBase(name) {
+  var transaction = connection.transaction(name, 'readonly');
+  var store = transaction.objectStore(name);
+  var req = store.count();
+  req.success = function(evt) {
+    console.log('There are ' + evt.target.result + ' elements in store ' + name);
+  };
+  req.onerror = function() {
+    console.log('request store count error');
+  };
+  req = store.openCursor();
+  req.onsuccess = function(evt) {
+    var cursor = evt.target.result;
+    if (cursor) {
+      console.debug("cursor.value:", cursor.value);
+      cursor.continue();
+    }
+  }
+  req.error = function(evt) {
+    console.log('request cursor error.');
+  }
+}
+
+function removeDataById(name, id) {
+  var transaction = connection.transaction(name, 'readwrite');
+  var store = transaction.objectStore(name);
+  var req = null;
+  if (id.length) {
+    req = store.delete(id);
+    req.onsuccess = function(evt) {
+      console.debug("evt.target:", evt.target);
+      console.debug("evt.target.result:", evt.target.result);
+      console.debug("delete successful");
+    };
+    req.onerror = function (evt) {
+      console.error("removeData:", evt.target.errorCode);
+    };    
+  }
+}
+
+getConnection();
+showDataBase(STORE.events);
+showDataBase(STORE.busytimes);
+showDataBase(STORE.alarms);
+showDataBase(STORE.settings);
+showDataBase(STORE.calendars);
+
+
+
+    ids.forEach(function(id) {
+      var trans = this.db.transaction('events');
+      var store = trans.objectStore('events');
+      var req = store.get(id);
+
+      req.onsuccess = success;
+      req.onerror = error;
+    }, this);
+
+
+var date = new Date(evt.target.value + 'T08:00:00');
