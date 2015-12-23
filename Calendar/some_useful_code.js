a26db698275f463f08d0d72dc846115af9248d62
xifei.wu <xifei.wu@acadine.com>
2015-12-23 10:40:04 +0800

    add some useful code.

--- Calendar/some_useful_code.js
+++ Calendar/some_useful_code.js
@@ -39,3 +39,16 @@ _onCalendarDisplayToggle: function(e) {
       switch(containerId) {
         case MONTH_VIEW:
           monthViewNavUpdate(elements);
+
+# get and set value in db Setting
+  var settings = this.app.store('Setting');
+  var layout = isAllDay ? 'allday' : 'standard';
+  settings.getValue(layout + 'AlarmDefault', next.bind(this));
+
+# data passed in dispatch of event_base.js
+data passed to dispatch:
+canonicalPath:;
+params: [id: 'local-first-d761afc0-3242-4725-a2ff-99b090f4662d-a6b99115-0649-4c03-85d7-91095e142fa6'];
+path: '/event/detail/local-first-d761afc0-3242-4725-a2ff-99b090f4662d-a6b99115-0649-4c03-85d7-91095e142fa6';
+pathname:
+title:
