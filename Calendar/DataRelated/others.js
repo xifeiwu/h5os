63cb2a1ead58bbf6225fddebc51fe30b8e96c099
xifei.wu <xifei.wu@acadine.com>
2015-12-23 10:45:35 +0800

    add more data information.

---
+++ Calendar/DataRelated/others.js
@@ -0,0 +1,8 @@
+# data passed in dispatch of event_base.js
+data passed to dispatch:
+
+canonicalPath:;
+params: [id: 'local-first-d761afc0-3242-4725-a2ff-99b090f4662d-a6b99115-0649-4c03-85d7-91095e142fa6'];
+path: '/event/detail/local-first-d761afc0-3242-4725-a2ff-99b090f4662d-a6b99115-0649-4c03-85d7-91095e142fa6';
+pathname:
+title:
