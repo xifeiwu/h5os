b39f60c557801262d5b5bdb9d67a9f9c4c739559
xifei.wu <xifei.wu@acadine.com>
2015-12-23 10:45:24 +0800

    need to do and some useful code.

--- Calendar/some_useful_code.js
+++ Calendar/some_useful_code.js
@@ -45,10 +45,3 @@ _onCalendarDisplayToggle: function(e) {
   var layout = isAllDay ? 'allday' : 'standard';
   settings.getValue(layout + 'AlarmDefault', next.bind(this));
 
-# data passed in dispatch of event_base.js
-data passed to dispatch:
-canonicalPath:;
-params: [id: 'local-first-d761afc0-3242-4725-a2ff-99b090f4662d-a6b99115-0649-4c03-85d7-91095e142fa6'];
-path: '/event/detail/local-first-d761afc0-3242-4725-a2ff-99b090f4662d-a6b99115-0649-4c03-85d7-91095e142fa6';
-pathname:
-title:
