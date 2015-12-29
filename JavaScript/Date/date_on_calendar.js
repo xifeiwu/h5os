440973497510339811541787be266eb35f775944
xifei.wu <xifei.wu@acadine.com>
2015-12-29 10:26:09 +0800

    add some summeries about date on calendar.

---
+++ JavaScript/Date/date_on_calendar.js
@@ -0,0 +1,11 @@
+the time in db is utc time.
+new Date('1969-12-26T03:00:00.000Z');
+Date 1969-12-26T03:00:00.000Z
+date = new Date('1969-12-26T03:00:00.000Z');
+Date 1969-12-26T03:00:00.000Z
+date.toLocaleString()
+"Thu Dec 25 16:00:00 1969"
+date.get
+undefined
+date.getDate();
+25
