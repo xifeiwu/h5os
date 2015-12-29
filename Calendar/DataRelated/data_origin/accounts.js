eb376a4801568781eac1535efed67eabe985b8e1
xifei.wu <xifei.wu@acadine.com>
2015-12-29 11:04:49 +0800

    stanardization db format of account and calendar.

--- Calendar/DataRelated/data_origin/accounts.js
+++ Calendar/DataRelated/data_origin/accounts.js
@@ -1,2 +1,20 @@
-cursor.value: {"entrypoint":"/","calendarHome":"/dav/xifei.wu/Calendar/","domain":"https://caldav.calendar.yahoo.com","password":"Acadinewxf2015","user":"xifei.wu@yahoo.com","providerType":"Caldav","preset":"yahoo","_id":1}
-cursor.value: {"entrypoint":"","calendarHome":"","domain":"","password":"","user":"","providerType":"Local","preset":"local","_id":"325c45e7-fbbb-420e-afde-d8fcdde17f34"}
+{
+  "entrypoint": "/",
+  "calendarHome": "/dav/xifei.wu/Calendar/",
+  "domain": "https://caldav.calendar.yahoo.com",
+  "password": "Acadinewxf2015",
+  "user": "xifei.wu@yahoo.com",
+  "providerType": "Caldav",
+  "preset": "yahoo",
+  "_id": 1
+}
+{
+  "entrypoint": "",
+  "calendarHome": "",
+  "domain": "",
+  "password": "",
+  "user": "",
+  "providerType": "Local",
+  "preset": "local",
+  "_id": "325c45e7-fbbb-420e-afde-d8fcdde17f34"
+}
--- Calendar/DataRelated/data_origin/calendars.js
+++ Calendar/DataRelated/data_origin/calendars.js
@@ -40,3 +40,45 @@
   "firstEventSyncDate": null,
   "_id": "local-first"
 }
+
+# add later for online local show
+{
+  "accountId": "b2cb3e3e-63c9-4fb5-889d-c1ea7f1c3c00",
+  "remote": {
+    "name": "333233566665555666651333333344661",
+    "id": "local-first",
+    "color": "#F97C17"
+  },
+  "color": "#00aacc",
+  "_id": 1
+}
+{
+  "remote": {
+    "id": "/dav/xifei.wu/Calendar/xifei%2520wu/",
+    "url": "/dav/xifei.wu/Calendar/xifei%2520wu/",
+    "name": "xifei wu",
+    "color": "#00aacc",
+    "description": "xifei.wu's Calendar: xifei wu",
+    "syncToken": "1-8",
+    "privilegeSet": ["read", "read-current-user-privilege-set", "write", "write-properties", "write-content", "bind", "unbind"]
+  },
+  "accountId": 1,
+  "localDisplayed": true,
+  "lastEventSyncDate": "2015-12-28T09:08:10.207Z",
+  "lastEventSyncToken": "1-8",
+  "firstEventSyncDate": "2015-12-27T10:00:00.000Z",
+  "_id": 2
+}
+{
+  "remote": {
+    "name": "Home",
+    "id": "local-first",
+    "color": "#f97c17"
+  },
+  "accountId": "b2cb3e3e-63c9-4fb5-889d-c1ea7f1c3c00",
+  "localDisplayed": true,
+  "lastEventSyncDate": "",
+  "lastEventSyncToken": "",
+  "firstEventSyncDate": null,
+  "_id": "local-first"
+}
