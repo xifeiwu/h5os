0958ccd7700d7d2175239a8f0e02b3a2dfe5267e
xifei.wu <xifei.wu@acadine.com>
2016-01-03 17:43:58 +0800

    add data structure of google account.

--- Calendar/DataRelated/data_origin/accounts.js
+++ Calendar/DataRelated/data_origin/accounts.js
@@ -1,4 +1,22 @@
 {
+  "entrypoint": "/caldav/v2/",
+  "calendarHome": "/caldav/v2/ytuwxf%40gmail.com/",
+  "domain": "https://apidata.googleusercontent.com",
+  "password": "",
+  "user": "ytuwxf@gmail.com",
+  "providerType": "Caldav",
+  "preset": "google",
+  "oauth": {
+    "access_token": "ya29.WwJQMyOQh2Lt07K6Di7_ASUGIoc73SR0d4QUky3gbcBigIafX2qyz-mmpp06d3DHZFgW",
+    "token_type": "Bearer",
+    "expires_in": 3600,
+    "id_token": "eyJhbGciOiJSUzI1NiIsImtpZCI6ImE3NDQ0YjU1ZjE4ZTJmYjQ2ZjYxZGJhY2ZhYjQxMzcwNjFjYTM1M2UifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXRfaGFzaCI6Ikw0QWZkRmExUEZZWk9rR1ZtMkNzNVEiLCJhdWQiOiIzNjgzNTQ4MjExMzctNWQyMXRwa3MwcmxkNjZsNjBwdTdraDNrb3Q0Y2ZhbnQuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTI1MTQ0OTEwODAyMjI4MDc5MDkiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXpwIjoiMzY4MzU0ODIxMTM3LTVkMjF0cGtzMHJsZDY2bDYwcHU3a2gza290NGNmYW50LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiZW1haWwiOiJ5dHV3eGZAZ21haWwuY29tIiwiaWF0IjoxNDUxNTUzMzIzLCJleHAiOjE0NTE1NTY5MjN9.Owb0vHYLSD9DvgCwS7XJOYXJS4ULZax6oeApAAECS1zHWAkqjt2wxkqomljT5M5z-pRqV24byWT28aFzn93aJB10J_LMdHyIKoyQK-Z7kFSxV4a5XqqD0wOLEMfp3oPQbD40DreQEzTSctn7DmhckD6j8aGL1yOFgKonEuYshLrOrG4q5FXqHSGRvpzNnEcsAo4oZfzjRP5co2KhuKZAxJHZZE9Zrb-YRM-d5aPz_hTZZKYeHeL-YUv7GmPFr6AO6JiBomFosZsW3Gm4xm4CSlR5PoDRNwaUsYsXyxUvdFb-sxpqESASVpt8zoq9IJqdIRFzSFuIgPa8FC_sWuZkPw",
+    "refresh_token": "1/gIRpdCaZXTrD7GNCMT0m0gKUnHP1E1svv87-uRkq3bVIgOrJDtdun6zK6XiATCKT",
+    "issued_at": 1451553323439
+  },
+  "_id": 1
+}
+{
   "entrypoint": "/",
   "calendarHome": "/dav/xifei.wu/Calendar/",
   "domain": "https://caldav.calendar.yahoo.com",
