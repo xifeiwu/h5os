02feff05fd44e77e5868fdcdda5a0630d9a72a62
xifei.wu <xifei.wu@acadine.com>
2016-01-04 11:46:10 +0800

    add online account model of yahoo in others.js

--- Calendar/DataRelated/others.js
+++ Calendar/DataRelated/others.js
@@ -6,3 +6,12 @@ params: [id: 'local-first-d761afc0-3242-4725-a2ff-99b090f4662d-a6b99115-0649-4c0
 path: '/event/detail/local-first-d761afc0-3242-4725-a2ff-99b090f4662d-a6b99115-0649-4c03-85d7-91095e142fa6';
 pathname:
 title:
+
+model of yahoo caldav account
+domain: "https://caldav.calendar.yahoo.com",
+entrypoint: "/",
+passwrod: "155***",
+preset: "yahoo",
+providerType: "Caldav",
+user: "xifei.wu@yahoo.com",
+usernameType: "email"
