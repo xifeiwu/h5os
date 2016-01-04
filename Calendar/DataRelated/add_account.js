4da1fc049e7e356a210aaefbf9f5b51793ab04df
xifei.wu <xifei.wu@acadine.com>
2016-01-04 19:45:27 +0800

    add debug message for yahoo add account

---
+++ Calendar/DataRelated/add_account.js
@@ -0,0 +1,9 @@
+worker/manager [calendar]  {"name":"worker","message":"worker/thread, [calendar] , \"data: {\\\"id\\\":0,\\\"role\\\":\\\"caldav\\\",\\\"payload\\\":[\\\"getAccount\\\",{\\\"entrypoint\\\":\\\"/\\\",\\\"calendarHome\\\":\\\"\\\",\\\"domain\\\":\\\"https://caldav.calendar.yahoo.com\\\",\\\"password\\\":\\\"15712872803\\\",\\\"user\\\":\\\"xifei.wu@yahoo.com\\\",\\\"providerType\\\":\\\"Caldav\\\",\\\"preset\\\":\\\"yahoo\\\"}]}\""}
+
+
+determineHttpStatusError
+worker.instance.postMessage(['_dispatch', data]);
+
+
+TypeError: this.accountList[calendar.accountId] is undefined lazy_loaded.js:2912:6
+TypeError: optionSelected is null lazy_loaded.js:3028:0
