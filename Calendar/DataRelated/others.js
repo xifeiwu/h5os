19ac83c7a4dcea4db9f2aecccda7abf815eca154
xifei.wu <xifei.wu@acadine.com>
2016-01-07 17:34:34 +0800

    add log for icalComponent.

--- Calendar/DataRelated/others.js
+++ Calendar/DataRelated/others.js
@@ -9,3 +9,89 @@ title:
 
 # location of Firefox Simulator in windows
 C:\Users\cos\AppData\Roaming\Mozilla\Firefox\Profiles\dt4vt9ta.default\extensions\fxos_2_2_simulator@mozilla.org\profile\webapps\calendar.gaiamobile.org
+
+# log of file js/provider/caldav_pull_events.js
+"add component" {
+    "eventId": "local-first-4586b8a7-b127-4e69-aea4-083069e8c151",
+    "lastRecurrenceId": {
+        "tzid": "America/Adak",
+        "offset": -36000000,
+        "utc": 1464238800000
+    },
+    "ical": "BEGIN:VCALENDAR\r\nPRODID:-//H5OS//Calendar 1.0//EN\r\nVERSION:2.0\r\nCALSCALE:GREGORIAN\r\nBEGIN:VTIMEZONE\r\nTZID:America/Adak\r\nBEGIN:STANDARD\r\nTZOFFSETFROM:-1000\r\nTZOFFSETTO:-1000\r\nDTSTART;TZID=America/Adak:20160301T050000\r\nEND:STANDARD\r\nEND:VTIMEZONE\r\nBEGIN:VEVENT\r\nDTSTART;TZID=America/Adak:20160301T050000\r\nDTEND;TZID=America/Adak:20160301T060000\r\nRRULE:FREQ=DAILY;INTERVAL=1\r\nDTSTAMP;TZID=America/Adak:20160106T044118\r\nUID:4586b8a7-b127-4e69-aea4-083069e8c151\r\nDESCRIPTION:\r\nLOCATION:\r\nSEQUENCE:1\r\nSTATUS:CONFIRMED\r\nSUMMARY:a\r\nTRANSP:TRANSPARENT\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n",
+    "iterator": {
+        "ruleIterators": [{
+            "initialized": true,
+            "rule": {
+                "freq": "DAILY",
+                "count": null,
+                "until": null,
+                "wkst": 2,
+                "interval": 1,
+                "parts": {}
+            },
+            "dtstart": {
+                "year": 2016,
+                "month": 3,
+                "day": 1,
+                "hour": 5,
+                "minute": 0,
+                "second": 0,
+                "isDate": false,
+                "timezone": "America/Adak"
+            },
+            "by_data": {
+                "BYSECOND": [0],
+                "BYMINUTE": [0],
+                "BYHOUR": [5]
+            },
+            "days": [],
+            "last": {
+                "year": 2016,
+                "month": 5,
+                "day": 27,
+                "hour": 5,
+                "minute": 0,
+                "second": 0,
+                "isDate": false,
+                "timezone": "America/Adak"
+            },
+            "by_indices": {
+                "BYSECOND": 0,
+                "BYMINUTE": 0,
+                "BYHOUR": 0,
+                "BYDAY": 0,
+                "BYMONTH": 0,
+                "BYWEEKNO": 0,
+                "BYMONTHDAY": 0
+            },
+            "occurrence_number": 88
+        }],
+        "ruleDates": [],
+        "exDates": [],
+        "ruleDateInc": 0,
+        "exDateInc": 0,
+        "last": {
+            "year": 2016,
+            "month": 5,
+            "day": 26,
+            "hour": 5,
+            "minute": 0,
+            "second": 0,
+            "isDate": false,
+            "timezone": "America/Adak"
+        },
+        "dtstart": {
+            "year": 2016,
+            "month": 3,
+            "day": 1,
+            "hour": 5,
+            "minute": 0,
+            "second": 0,
+            "isDate": false,
+            "timezone": "America/Adak"
+        },
+        "complete": false
+    },
+    "calendarId": "local-first"
+}
