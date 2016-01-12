19d8351bdc7d4db9b251ab01101c383f58de82f3
xifei.wu <xifei.wu@acadine.com>
2016-01-12 15:25:47 +0800

    fix model basic.html

--- JavaScript/Basic/basic.js
+++ JavaScript/Basic/basic.js
@@ -5,3 +5,15 @@ if (obj && obj.prop) {
   console.log('obj.prop no exist.');
 }
 
+obj2 = {};
+obj2['name'] = 'This is the name of obj2';
+console.log(obj2['name']);
+console.log(obj2['age']);
+
+function testError(err) {
+  if (!err) {
+    console.log('callback success.');
+  } else {
+    console.log('callback fail.');
+  }
+}
