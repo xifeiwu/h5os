7c269f679dae80933c3f3e12c30f71444942f12e
xifei.wu <xifei.wu@acadine.com>
2016-01-28 19:16:27 +0800

    add file summery.js

---
+++ Calendar/summery.js
@@ -0,0 +1,17 @@
+# About focus and blur
+Try to focus on an element in its blur listener function always end with fail.
+If focus is called in nextTick funciton, it works well.
+function nextTick(callback) {
+  Promise.resolve().then(callback);
+}
+below are comments copied from h5-input-wrapper. refocus should after blur.
+// We need to do setTimeout because both finishTextEdit and finishSelect
+// moves focus to the input wrapper and which causes the "focus" event
+// occurs before the original "blur" event from the perspective of soft key
+// helper and clears all of the soft keys.
+
+# Something need to notice when js coding
+the flow of event propagation.
+refocus should after blur all-complete.
+some asynchronous function may cause incorrect work sequence.
+transaction of indexedDB should execute one after another. 
