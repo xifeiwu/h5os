3011a3294206d404b86006c313114660ce146068
xifei.wu <xifei.wu@acadine.com>
2016-02-24 11:42:09 +0800

    add log about recurring event in start_up_log.js

--- Calendar/start_up_log.js
+++ Calendar/start_up_log.js
@@ -84,10 +84,140 @@ controllers/recurring_events [calendar]  "Will try to complete expansion (tries 
     at module.exports/< (app://calendar.gaiamobile.org/js/bundle.js:3016:4) bundle.js:3016:4
 controllers/recurring_events [calendar]  "Expansion attempt did expand:" false 
     at module.exports/< (app://calendar.gaiamobile.org/js/bundle.js:3016:4)
-arrow function的使用，calendar chooser在onactive的时候更新checkbox的状态，提交pr。了解modify event中关于date的逻辑，将提示改为dialog，提交相关pr。了解MDN localization，data-l10n-args,data-l10n-id,data-l10n-date-format的使用。看Alarm的template及相关pr。看禾一提交的关于setup calendar的pr，构思对h5-dialog input blur的处理，在skype上咨询相关问题，看h5-input－wrapper。
-通过skype沟通及代码调试了解input上的softkey与IME softkey的关系，input的softkey会被覆盖，但addEventListener可以使用，了解了h5-input－wrapper的部分作用。添加空calendar会在dailog上给出提示，提交相关pr。看css transform、animation等。看数据库操作相关代码，写使用index及IDBKeyRange的demo，构思rename calendar bug的解决方案。贴报销单。
-借力很重要，基础组件很重要。
-团队可以相互借鉴。个人闷头思路窄，做很容易进入死胡同。
-经验很重要。
-在一个大项目上深耕比将经历分散到多个小项目要好。
-利用现有库。
+
+#The log of add an recurring events.
+worker/thread [calendar]  "Will listen for messages from the main thread..." caldav_worker.js:1473:4
+worker/manager [calendar]  {"name":"worker","message":"worker/initialize, [calendar] , \"Will create new caldav service...\""} bundle.js:2429:4
+worker/manager [calendar]  {"name":"worker","message":"service/caldav, [calendar] , \"Will listen for worker events...\""} bundle.js:2429:4
+worker/manager [calendar]  {"name":"worker","message":"service/caldav, [calendar] , \"Worker will fulfill\", \"streamEventsFromLocal\", \"request...\""} bundle.js:2429:4
+worker/manager [calendar]  {"name":"worker","message":"service/caldav, [calendar] , \"alarm time\", \"EF\", \"will add -1\", \"start:\", \"Thu Feb 25 2016 06:00:00 GMT+0300 (AST)\", \"end:\", \"Thu Feb 25 2016 07:00:00 GMT+0300 (AST)\", \"now:\", \"Fri Feb 26 2016 05:48:19 GMT+0300 (AST)\""} bundle.js:2429:4
+pull events [calendar]  "add event" {"calendarId":"local-first","remote":{"alarms":[],"id":"b1198f9b-4348-42ed-9038-867aeb26aaa5","title":"EF","recurrenceId":null,"isRecurring":true,"repeat":"every-week","description":"","location":"","start":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1456380000000},"end":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1456383600000},"timeStamp":1456454899268},"_id":"local-first-b1198f9b-4348-42ed-9038-867aeb26aaa5"} bundle.js:2429:4
+pull events [calendar]  "add component" {"eventId":"local-first-b1198f9b-4348-42ed-9038-867aeb26aaa5"} bundle.js:2429:4
+pull events [calendar]  "add component" {"eventId":"local-first-b1198f9b-4348-42ed-9038-867aeb26aaa5","ical":"BEGIN:VCALENDAR\r\nPRODID:-//H5OS//Calendar 1.0//EN\r\nVERSION:2.0\r\nBEGIN:VTIMEZONE\r\nTZID:Asia/Baghdad\r\nBEGIN:STANDARD\r\nTZOFFSETFROM:+0300\r\nTZOFFSETTO:+0300\r\nDTSTART:20160225T060000\r\nEND:STANDARD\r\nEND:VTIMEZONE\r\nBEGIN:VEVENT\r\nDTSTART;TZID=Asia/Baghdad:20160225T060000\r\nDTEND;TZID=Asia/Baghdad:20160225T070000\r\nRRULE:FREQ=WEEKLY;BYDAY=TH;UNTIL=20160302T060000\r\nDTSTAMP;TZID=Asia/Baghdad:20160226T054756\r\nUID:b1198f9b-4348-42ed-9038-867aeb26aaa5\r\nDESCRIPTION:\r\nLOCATION:\r\nSEQUENCE:0\r\nSTATUS:CONFIRMED\r\nSUMMARY:EF\r\nTRANSP:OPAQUE\r\nEND:VEVENT\r\nEND:VCALENDAR","iterator":{"ruleIterators":[],"ruleDates":[],"exDates":[],"ruleDateInc":0,"exDateInc":0,"last":{"year":2016,"month":2,"day":25,"hour":6,"minute":0,"second":0,"isDate":false,"timezone":"Asia/Baghdad"},"dtstart":{"year":2016,"month":2,"day":25,"hour":6,"minute":0,"second":0,"isDate":false,"timezone":"Asia/Baghdad"},"complete":true},"calendarId":"local-first"} bundle.js:2429:4
+pull events [calendar]  "add busytime" {"start":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1456380000000},"end":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1456383600000},"recurrenceId":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1456380000000},"eventId":"local-first-b1198f9b-4348-42ed-9038-867aeb26aaa5","isException":false,"_id":"local-first-b1198f9b-4348-42ed-9038-867aeb26aaa5-367d9002-edc4-4b8e-ab2c-6a76edc56bb2","calendarId":"local-first","startDate":"2016-02-25T03:00:00.000Z","endDate":"2016-02-25T04:00:00.000Z"}
+
+# The value of exception event and its related recurring event.
+cursor.value: {"remote":{"start":{"utc":1461247200000,"offset":10800000},"startDate":"2016-04-21T11:00:00.000Z","end":{"utc":1461596400000,"offset":10800000},"endDate":"2016-04-25T12:00:00.000Z","title":"3","location":"","repeat":"never","description":"","recurrenceId":"","timeStamp":1456394411660,"isRecurring":false,"alarms":[],"color":"#6115BF","id":"099bba36-3563-4487-a5f6-c836957ca956"},"calendarId":"local-first","_id":"local-first-099bba36-3563-4487-a5f6-c836957ca956"}
+cursor.value: {"calendarId":"local-first","remote":{"alarms":[],"id":"f61c7f79-90bc-4215-b310-83ef471e21fc","title":"Be","recurrenceId":null,"isRecurring":true,"repeat":"every-week","description":"","location":"","start":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1461160800000},"end":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1461164400000},"timeStamp":1456394659474},"_id":"local-first-f61c7f79-90bc-4215-b310-83ef471e21fc"}
+cursor.value: {"calendarId":"local-first","remote":{"alarms":[],"id":"f61c7f79-90bc-4215-b310-83ef471e21fc","title":"Bea","recurrenceId":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1462370400000},"isRecurring":false,"repeat":"never","description":"","location":"","start":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1462370400000},"end":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1462374000000},"timeStamp":1456394659468},"parentId":"local-first-f61c7f79-90bc-4215-b310-83ef471e21fc","_id":"local-first-f61c7f79-90bc-4215-b310-83ef471e21fc-1462370400000"}
+
+
+# The origin recurring events and its updateAllFuture recurring events
+There are 2 elements in store events
+cursor.value: {"calendarId":"local-first","remote":{"alarms":[],"id":"6c25cc27-82ee-4ca4-99c2-066acf4f4dbe","title":"Add","recurrenceId":null,"isRecurring":true,"repeat":"every-week","description":"","location":"","start":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1456984800000},"end":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1456988400000},"timeStamp":1456455275417},"_id":"local-first-6c25cc27-82ee-4ca4-99c2-066acf4f4dbe"}
+cursor.value: {"calendarId":"local-first","remote":{"alarms":[],"id":"a04e9752-1c27-410c-9a3d-5d4d85fce8cd","title":"Ad","recurrenceId":null,"isRecurring":true,"repeat":"every-week","description":"","location":"","start":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1456380000000},"end":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1456383600000},"timeStamp":1456455274355},"_id":"local-first-a04e9752-1c27-410c-9a3d-5d4d85fce8cd"}
+
+There are 2 elements in store icalComponents
+cursor.value: {"eventId":"local-first-6c25cc27-82ee-4ca4-99c2-066acf4f4dbe","lastRecurrenceId":{"tzid":"Asia/Baghdad","offset":10800000,"utc":1469685600000},"ical":"BEGIN:VCALENDAR\r\nPRODID:-//H5OS//Calendar 1.0//EN\r\nVERSION:2.0\r\nBEGIN:VTIMEZONE\r\nTZID:Asia/Baghdad\r\nBEGIN:STANDARD\r\nTZOFFSETFROM:+0300\r\nTZOFFSETTO:+0300\r\nDTSTART:20160303T060000\r\nEND:STANDARD\r\nEND:VTIMEZONE\r\nBEGIN:VEVENT\r\nDTSTART;TZID=Asia/Baghdad:20160303T060000\r\nDTEND;TZID=Asia/Baghdad:20160303T070000\r\nRRULE:FREQ=WEEKLY;BYDAY=TH\r\nDTSTAMP;TZID=Asia/Baghdad:20160226T055434\r\nUID:6c25cc27-82ee-4ca4-99c2-066acf4f4dbe\r\nDESCRIPTION:\r\nLOCATION:\r\nSEQUENCE:0\r\nSTATUS:CONFIRMED\r\nSUMMARY:Add\r\nTRANSP:OPAQUE\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n","iterator":{"ruleIterators":[{"initialized":true,"rule":{"freq":"WEEKLY","byday":"TH"},"dtstart":{"year":2016,"month":3,"day":3,"hour":6,"minute":0,"second":0,"isDate":false,"timezone":"Asia/Baghdad"},"by_data":{"BYDAY":["TH"],"BYSECOND":[0],"BYMINUTE":[0],"BYHOUR":[6],"BYMONTHDAY":[3]},"days":[],"last":{"year":2016,"month":8,"day":4,"hour":6,"minute":0,"second":0,"isDate":false,"timezone":"Asia/Baghdad"},"by_indices":{"BYSECOND":0,"BYMINUTE":0,"BYHOUR":0,"BYDAY":0,"BYMONTH":0,"BYWEEKNO":0,"BYMONTHDAY":0},"occurrence_number":23}],"ruleDates":[],"exDates":[],"ruleDateInc":0,"exDateInc":0,"last":{"year":2016,"month":7,"day":28,"hour":6,"minute":0,"second":0,"isDate":false,"timezone":"Asia/Baghdad"},"dtstart":{"year":2016,"month":3,"day":3,"hour":6,"minute":0,"second":0,"isDate":false,"timezone":"Asia/Baghdad"},"complete":false},"calendarId":"local-first"}
+cursor.value: {"eventId":"local-first-a04e9752-1c27-410c-9a3d-5d4d85fce8cd","ical":"BEGIN:VCALENDAR\r\nPRODID:-//H5OS//Calendar 1.0//EN\r\nVERSION:2.0\r\nBEGIN:VTIMEZONE\r\nTZID:Asia/Baghdad\r\nBEGIN:STANDARD\r\nTZOFFSETFROM:+0300\r\nTZOFFSETTO:+0300\r\nDTSTART:20160225T060000\r\nEND:STANDARD\r\nEND:VTIMEZONE\r\nBEGIN:VEVENT\r\nDTSTART;TZID=Asia/Baghdad:20160225T060000\r\nDTEND;TZID=Asia/Baghdad:20160225T070000\r\nRRULE:FREQ=WEEKLY;BYDAY=TH;UNTIL=20160302T060000\r\nDTSTAMP;TZID=Asia/Baghdad:20160226T055407\r\nUID:a04e9752-1c27-410c-9a3d-5d4d85fce8cd\r\nDESCRIPTION:\r\nLOCATION:\r\nSEQUENCE:0\r\nSTATUS:CONFIRMED\r\nSUMMARY:Ad\r\nTRANSP:OPAQUE\r\nEND:VEVENT\r\nEND:VCALENDAR","iterator":{"ruleIterators":[],"ruleDates":[],"exDates":[],"ruleDateInc":0,"exDateInc":0,"last":{"year":2016,"month":2,"day":25,"hour":6,"minute":0,"second":0,"isDate":false,"timezone":"Asia/Baghdad"},"dtstart":{"year":2016,"month":2,"day":25,"hour":6,"minute":0,"second":0,"isDate":false,"timezone":"Asia/Baghdad"},"complete":true},"calendarId":"local-first"}
+the transaction is complete.
+
+# event and icalComponent
+{
+    "calendarId": "local-first",
+    "remote": {
+        "alarms": [],
+        "id": "903bb914-a4d8-444c-9d71-1c2ca1695d90",
+        "title": "D",
+        "recurrenceId": null,
+        "isRecurring": true,
+        "repeat": "every-week",
+        "description": "",
+        "location": "",
+        "start": {
+            "tzid": "America/Adak",
+            "offset": -36000000,
+            "utc": 1458763200000
+        },
+        "end": {
+            "tzid": "America/Adak",
+            "offset": -36000000,
+            "utc": 1458766800000
+        },
+        "timeStamp": 1456378796167
+    },
+    "_id": "local-first-903bb914-a4d8-444c-9d71-1c2ca1695d90"
+}
+
+{
+    "eventId": "local-first-903bb914-a4d8-444c-9d71-1c2ca1695d90",
+    "lastRecurrenceId": {
+        "tzid": "America/Adak",
+        "offset": -36000000,
+        "utc": 1469649600000
+    },
+    "ical": "BEGIN:VCALENDAR\r\nPRODID:-//H5OS//Calendar 1.0//EN\r\nVERSION:2.0\r\nBEGIN:VTIMEZONE\r\nTZID:America/Adak\r\nBEGIN:STANDARD\r\nTZOFFSETFROM:-1000\r\nTZOFFSETTO:-1000\r\nDTSTART:20160323T200000\r\nEND:STANDARD\r\nEND:VTIMEZONE\r\nBEGIN:VEVENT\r\nDTSTART;TZID=America/Adak:20160323T200000\r\nDTEND;TZID=America/Adak:20160323T210000\r\nRRULE:FREQ=WEEKLY;BYDAY=WE\r\nDTSTAMP;TZID=America/Adak:20160224T193955\r\nUID:903bb914-a4d8-444c-9d71-1c2ca1695d90\r\nDESCRIPTION:\r\nLOCATION:\r\nSEQUENCE:0\r\nSTATUS:CONFIRMED\r\nSUMMARY:D\r\nTRANSP:OPAQUE\r\nEND:VEVENT\r\nEND:VCALENDAR\r\n",
+    "iterator": {
+        "ruleIterators": [{
+            "initialized": true,
+            "rule": {
+                "freq": "WEEKLY",
+                "byday": "WE"
+            },
+            "dtstart": {
+                "year": 2016,
+                "month": 3,
+                "day": 23,
+                "hour": 20,
+                "minute": 0,
+                "second": 0,
+                "isDate": false,
+                "timezone": "America/Adak"
+            },
+            "by_data": {
+                "BYDAY": ["WE"],
+                "BYSECOND": [0],
+                "BYMINUTE": [0],
+                "BYHOUR": [20],
+                "BYMONTHDAY": [23]
+            },
+            "days": [],
+            "last": {
+                "year": 2016,
+                "month": 8,
+                "day": 3,
+                "hour": 20,
+                "minute": 0,
+                "second": 0,
+                "isDate": false,
+                "timezone": "America/Adak"
+            },
+            "by_indices": {
+                "BYSECOND": 0,
+                "BYMINUTE": 0,
+                "BYHOUR": 0,
+                "BYDAY": 0,
+                "BYMONTH": 0,
+                "BYWEEKNO": 0,
+                "BYMONTHDAY": 0
+            },
+            "occurrence_number": 20
+        }],
+        "ruleDates": [],
+        "exDates": [],
+        "ruleDateInc": 0,
+        "exDateInc": 0,
+        "last": {
+            "year": 2016,
+            "month": 7,
+            "day": 27,
+            "hour": 20,
+            "minute": 0,
+            "second": 0,
+            "isDate": false,
+            "timezone": "America/Adak"
+        },
+        "dtstart": {
+            "year": 2016,
+            "month": 3,
+            "day": 23,
+            "hour": 20,
+            "minute": 0,
+            "second": 0,
+            "isDate": false,
+            "timezone": "America/Adak"
+        },
+        "complete": false
+    },
+    "calendarId": "local-first"
+}
