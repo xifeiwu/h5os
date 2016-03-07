c226ed9ba21ca0e54e5491448aca89f21792d94d
xifei.wu <xifei.wu@acadine.com>
2016-03-07 14:42:02 +0800

    fix demo for apply call and bind.

--- JavaScript/Basic/ApplyCallBind/apply_call_diff.js
+++ JavaScript/Basic/ApplyCallBind/apply_call_diff.js
@@ -1,10 +1,18 @@
 function alert() {
-  console.log(arguments);
+  // console.log(arguments);
+  console.log('The length of arguments: ' + arguments.length);
+  var length = arguments.length;
+  var i=0;
+  for (; i < length; i++) {
+    console.log(arguments[i]);
+  }
 }
 function a(xx, yy) {    
-    alert(xx, yy);    
-    alert(this);    
+    alert(xx, yy);  
     alert(arguments);
 }
 a.apply(null, [5, 55]);
 a.call(null, [5, 55]);
+console.log('=============================');
+a.apply(null, 100);
+a.call(null, 100);
