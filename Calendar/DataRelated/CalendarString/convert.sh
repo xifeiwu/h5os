1300e04aa13d32dcf0a609cf5661513088771e72
xifei.wu <xifei.wu@acadine.com>
2016-03-14 16:07:52 +0800

    update conver.sh

--- Calendar/DataRelated/CalendarString/convert.sh
+++ Calendar/DataRelated/CalendarString/convert.sh
@@ -2,16 +2,31 @@
 echo
 while read line
 do
-  if [ "${line}" ]; then
-    printf '%s%s' "${line}" '\r\n'
-  fi
+ if [ "${line}" ]; then
+   printf '%s%s' "${line}" '\r\n'
+ fi
 done < ${1}
 echo
 
-#content=`cat ${1}`
-#string=''
-#for line in ${content}
-#do
-#  string=${string}${line}\\r\\n
-#done
-#printf '%s' "${string}" 
+# content=`cat ${1}`
+# string=''
+# for line in ${content}
+# do
+#   string=${string}${line}\\r\\n
+# done
+# printf '%s' "${string}" 
+
+# There will be error if space letter in string
+# content=`cat $1`
+# string=''
+# count=0
+# for line in ${content}
+# do
+#   # ((count++))
+#   # echo ${count} ${line}
+#   string=${string}${line}\\r\\n
+# done
+# echo
+# printf '%s' "${string}"
+# echo
+# echo
