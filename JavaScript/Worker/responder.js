17264d2b329f0d3db437d6bcd1e128d30d44c46f
xifei.wu <xifei.wu@acadine.com>
2016-03-23 15:36:32 +0800

    add demo to post object to worker from main thread.

--- JavaScript/Worker/responder.js
+++ JavaScript/Worker/responder.js
@@ -3,5 +3,6 @@ postMessage('responder has started...');
 // Setup an event listener that will handle messages sent to the worker.
 self.addEventListener('message', function(e) {
   // Send the message back.
-  self.postMessage('You said: ' + e.data);
+  var data = e.data;
+  self.postMessage('You said: ' + data);
 }, false);
