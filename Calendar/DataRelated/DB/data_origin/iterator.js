33caa946273201587923406538ae51e7b7b21829
xifei.wu <xifei.wu@acadine.com>
2016-03-23 15:29:12 +0800

    add data structure of iterator

---
+++ Calendar/DataRelated/DB/data_origin/iterator.js
@@ -0,0 +1,281 @@
+{
+  "ruleIterators": [{
+    "initialized": true,
+    "rule": {
+      "freq": "DAILY"
+    },
+    "dtstart": {
+      "year": 2016,
+      "month": 1,
+      "day": 1,
+      "hour": 1,
+      "minute": 0,
+      "second": 0,
+      "isDate": false,
+      "timezone": "Asia/Shanghai"
+    },
+    "by_data": {
+      "BYSECOND": [0],
+      "BYMINUTE": [0],
+      "BYHOUR": [1]
+    },
+    "days": [],
+    "last": {
+      "year": 2016,
+      "month": 1,
+      "day": 1,
+      "hour": 1,
+      "minute": 0,
+      "second": 0,
+      "isDate": false,
+      "timezone": "Asia/Shanghai"
+    },
+    "by_indices": {
+      "BYSECOND": 0,
+      "BYMINUTE": 0,
+      "BYHOUR": 0,
+      "BYDAY": 0,
+      "BYMONTH": 0,
+      "BYWEEKNO": 0,
+      "BYMONTHDAY": 0
+    },
+    "occurrence_number": 1
+  }],
+  "ruleDates": [],
+  "exDates": [],
+  "ruleDateInc": 0,
+  "exDateInc": 0,
+  "last": {
+    "year": 2016,
+    "month": 1,
+    "day": 1,
+    "hour": 1,
+    "minute": 0,
+    "second": 0,
+    "isDate": false,
+    "timezone": "Asia/Shanghai"
+  },
+  "dtstart": {
+    "year": 2016,
+    "month": 1,
+    "day": 1,
+    "hour": 1,
+    "minute": 0,
+    "second": 0,
+    "isDate": false,
+    "timezone": "Asia/Shanghai"
+  },
+  "complete": false
+}
+
+> after expanding
+{
+  "ruleIterators": [{
+    "initialized": true,
+    "rule": {
+      "freq": "DAILY"
+    },
+    "dtstart": {
+      "year": 2016,
+      "month": 1,
+      "day": 1,
+      "hour": 1,
+      "minute": 0,
+      "second": 0,
+      "isDate": false,
+      "timezone": "Asia/Shanghai"
+    },
+    "by_data": {
+      "BYSECOND": [0],
+      "BYMINUTE": [0],
+      "BYHOUR": [1]
+    },
+    "days": [],
+    "last": {
+      "year": 2016,
+      "month": 1,
+      "day": 13,
+      "hour": 1,
+      "minute": 0,
+      "second": 0,
+      "isDate": false,
+      "timezone": "Asia/Shanghai"
+    },
+    "by_indices": {
+      "BYSECOND": 0,
+      "BYMINUTE": 0,
+      "BYHOUR": 0,
+      "BYDAY": 0,
+      "BYMONTH": 0,
+      "BYWEEKNO": 0,
+      "BYMONTHDAY": 0
+    },
+    "occurrence_number": 13
+  }],
+  "ruleDates": [],
+  "exDates": [],
+  "ruleDateInc": 0,
+  "exDateInc": 0,
+  "last": {
+    "year": 2016,
+    "month": 1,
+    "day": 12,
+    "hour": 1,
+    "minute": 0,
+    "second": 0,
+    "isDate": false,
+    "timezone": "Asia/Shanghai"
+  },
+  "dtstart": {
+    "year": 2016,
+    "month": 1,
+    "day": 1,
+    "hour": 1,
+    "minute": 0,
+    "second": 0,
+    "isDate": false,
+    "timezone": "Asia/Shanghai"
+  },
+  "complete": false
+}
+{
+  ruleIterators: [{
+    initialized: true,
+    rule: {
+      freq: 'DAILY'
+    },
+    dtstart: {
+      year: 2016,
+      month: 1,
+      day: 1,
+      hour: 1,
+      minute: 0,
+      second: 0,
+      isDate: false,
+      timezone: 'Asia/Shanghai'
+    },
+    by_data: {
+      BYSECOND: [0],
+      BYMINUTE: [0],
+      BYHOUR: [1]
+    },
+    days: [],
+    last: {
+      year: 2016,
+      month: 1,
+      day: 13,
+      hour: 1,
+      minute: 0,
+      second: 0,
+      isDate: false,
+      timezone: 'Asia/Shanghai'
+    },
+    by_indices: {
+      BYSECOND: 0,
+      BYMINUTE: 0,
+      BYHOUR: 0,
+      BYDAY: 0,
+      BYMONTH: 0,
+      BYWEEKNO: 0,
+      BYMONTHDAY: 0
+    },
+    occurrence_number: 13
+  }],
+  ruleDates: [],
+  exDates: [],
+  ruleDateInc: 0,
+  exDateInc: 0,
+  last: {
+    year: 2016,
+    month: 1,
+    day: 12,
+    hour: 1,
+    minute: 0,
+    second: 0,
+    isDate: false,
+    timezone: 'Asia/Shanghai'
+  },
+  dtstart: {
+    year: 2016,
+    month: 1,
+    day: 1,
+    hour: 1,
+    minute: 0,
+    second: 0,
+    isDate: false,
+    timezone: 'Asia/Shanghai'
+  },
+  complete: false
+}
+
+
+> iterator object
+var iterator = {
+ruleIterators: [{
+  initialized: true,
+  rule: {
+    freq: 'DAILY'
+  },
+  dtstart: {
+    year: 2016,
+    month: 1,
+    day: 1,
+    hour: 1,
+    minute: 0,
+    second: 0,
+    isDate: false,
+    timezone: 'Asia/Shanghai'
+  },
+  by_data: {
+    BYSECOND: [0],
+    BYMINUTE: [0],
+    BYHOUR: [1]
+  },
+  days: [],
+  last: {
+    year: 2020,
+    month: 8,
+    day: 26,
+    hour: 1,
+    minute: 0,
+    second: 0,
+    isDate: false,
+    timezone: 'Asia/Shanghai'
+  },
+  by_indices: {
+    BYSECOND: 0,
+    BYMINUTE: 0,
+    BYHOUR: 0,
+    BYDAY: 0,
+    BYMONTH: 0,
+    BYWEEKNO: 0,
+    BYMONTHDAY: 0
+  },
+  occurrence_number: 1700
+}],
+ruleDates: [],
+exDates: [],
+ruleDateInc: 0,
+exDateInc: 0,
+last: {
+  year: 2020,
+  month: 8,
+  day: 25,
+  hour: 1,
+  minute: 0,
+  second: 0,
+  isDate: false,
+  timezone: 'Asia/Shanghai'
+},
+dtstart: {
+  year: 2016,
+  month: 1,
+  day: 1,
+  hour: 1,
+  minute: 0,
+  second: 0,
+  isDate: false,
+  timezone: 'Asia/Shanghai'
+},
+complete: false
+};
