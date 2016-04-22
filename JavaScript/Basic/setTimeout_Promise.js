358f3357137ec46b7f96a3472cbf4846732c750c
xifei.wu <xifei.wu@acadine.com>
2016-04-22 19:12:23 +0800

    demo for setTimeout and Promise.

---
+++ JavaScript/Basic/setTimeout_Promise.js
@@ -0,0 +1,27 @@
+setTimeout(function(){
+    console.log(1);
+},0);
+setTimeout(function(){
+    console.log(2);
+},0);
+new Promise(function(resolve){
+    console.log(3);
+    resolve();
+    console.log(4);
+}).then(function(){
+    console.log(5);
+});
+console.log(6);
+console.log(7);
+
+setTimeout(function(){console.log(4)},0);
+new Promise(function(resolve){
+    console.log(1)
+    for( var i=0 ; i<10000 ; i++ ){
+        i==9999 && resolve()
+    }
+    console.log(2)
+}).then(function(){
+    console.log(5)
+});
+console.log(3);
